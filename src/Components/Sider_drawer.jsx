7975cb68c90dfbfe36e3309025d95eb93ca40d1c
ahsanimran54080 <ahxanimran456@gmail.com>
2023-08-31 23:28:38 +0500

    updating more

--- src/Components/Sider_drawer.jsx
+++ src/Components/Sider_drawer.jsx
@@ -1,23 +1,55 @@
 import { Drawer } from "antd";
 import { MainaddproductContext } from "../Context/AddProducts";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import { BsPlus } from "react-icons/bs";
+import { AiOutlineMinus } from "react-icons/ai";
 
 function SideDrawer({ close, Open, selectedItems }) {
     const { addproduct } = useContext(MainaddproductContext);
+    const [Quantity, setQuantity] = useState(1);
+    const [Price, setPrice] = useState();
+    const [addtocart_products, setaddtocart_products] = useState();
+    useEffect(() => {
+        if (addproduct.length > 0) {
+            setaddtocart_products(addproduct)
+        }
+    }, [addproduct]);
     const onClose = () => {
         close(false);
     };
+    const handlePlus = (item) => {
+        if (Quantity < 3) {
+            const selected = addtocart_products.filter((item) => item.id == item.id)
+            console.log(selected);
+                setQuantity((prev) => prev + 1)
+        }
+    }
+    const handleMinus = () => {
+        if (Quantity > 1) {
+            setQuantity((prev) => prev - 1)
+        }
+    }
 
     return (
         <Drawer title="Shopping cart" placement="right" onClose={onClose} open={Open}>
-            {Array.isArray(addproduct) && addproduct.length > 0 ? addproduct.map((items, index) => {
+            {Array.isArray(addtocart_products) && addtocart_products.length > 0 ? addtocart_products.map((items, index) => {
                 return (
-                    <div className="row justify-content-center align-center" key={index}>
-                        <div className="col-md-2 mb-2" >
-                            <img className="img-fluid rounded " src={items.imgurl} alt="" />
+                    <div className="row align-items-center shopping-cart-slider  m-0" key={index}>
+                        <div className="col-md-2 text-center p-0 ">
+                            <img className="img-fluid " src={items.imgurl} alt="" />
                         </div>
-                        <div className="col-md-10 mb-2" style={{ display: "flex", alignItems: "center" }}>
-                            <p className="m-0">{items.description}</p>
+                        <div className="col-md-10" style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+                            <div>
+                                <p className="m-0 p-0">{items.description}</p>
+                            </div>
+                            <div className="d-flex justify-content-between align-items-center gap-3">
+                                <AiOutlineMinus onClick={() => handleMinus(items)} cursor={"pointer"} />
+                                <p className="p-0 m-0">{Quantity}</p>
+                                <BsPlus onClick={() => handlePlus(items)} cursor={"pointer"} />
+                            </div>
+                            <div>
+                                <p className="p-0 m-0">{items.Rates}</p>
+                            </div>
                         </div>
                     </div>
                 )
